2d125dafb90e05df01f784d44b4f5bd5f5c4a900
sam_dev <samueldevpro09@gmail.com>
2022-08-26 01:16:41 +0200

    first commit

---
+++ scraper/index.js
@@ -0,0 +1,55 @@
+const puppeteer = require("puppeteer-extra");
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1840,
+    height: 1080,
+    deviceScaleFactor: 1,
+  });
+  try {
+    await page.goto("https://www.youtube.com/watch?v=CuWowKXsNHg", {
+      waitUntil: "networkidle2",
+    });
+
+    // wait 1sec
+    await page.waitForTimeout(2000);
+
+    const button1 = await page.$(
+      "#button.dropdown-trigger.style-scope.ytd-menu-renderer"
+    );
+    await button1.evaluate((b) => b.click());
+
+    await page.waitForTimeout(500);
+
+    const button2 = await page.$$(".style-scope.ytd-menu-popup-renderer");
+    await button2[2].evaluate((b) => b.click());
+
+    await page.waitForTimeout(2000);
+
+    const yt = await page.evaluate(() =>
+      Array.from(
+        document.querySelectorAll(
+          ".style-scope.ytd-transcript-search-panel-renderer"
+        )
+      )[2]
+        .innerText.trim()
+        .split("\n")
+    );
+
+    console.log(yt);
+  } catch (error) {
+    console.log(`${error}`);
+  }
+
+  // close browser
+  await browser.close();
+})();
